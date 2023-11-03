122d4e652c32d5809d0aa4c028239aaf393bac6d
TCISERDAROGLU <okanserdaroglu2@gmail.com>
2023-11-03 23:53:43 +0300

    showSnackBar

--- app/src/main/java/com/example/composework/MainActivity.kt
+++ app/src/main/java/com/example/composework/MainActivity.kt
@@ -3,20 +3,77 @@ package com.example.composework
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.composework.ui.theme.ComposeWorkTheme
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
+
+/**
+ * in here we make a column and make it scrollable, we add it a scrollState
+ * because we need to keep a scroll state to chance the position of our scrollable item.
+ * we have a default scrollState and use it.
+ * ------
+ * With lazy column it has more performance because it loads lazily. Lazy column lists only visible items
+ * in screen that's why you can have better performance then normal column
+ *
+ *
+ */
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            //ListWithColumn()
+            ListWithLazyColumn()
+        }
+    }
+}
 
+@Composable
+fun ListWithColumn() {
+    val scrollState = rememberScrollState()
+    Column(
+        modifier = Modifier
+            .verticalScroll(scrollState)
+            .fillMaxSize()
+    ) {
+        for (i in 1..50) {
+            Text(
+                text = "Item $i",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = 16.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun ListWithLazyColumn() {
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        itemsIndexed((arrayListOf("This","is","Jetpack","Compose"))) { _, item ->
+            Text(
+                text = item,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = 16.dp)
+            )
         }
     }
 }
